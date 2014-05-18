19254ed2afd76969ff3b2cd2bfddfad02542a557
Jason Mannon <jmannonpdx@gmail.com>
2014-05-18 12:39:51 -0700

    initial commit, created database

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Portlandbikeswap::Application.config.secret_key_base = '2550db6fa99bce521815c5154fe62b74e25f68a67da11951b767146ae9daa3d0d24fdd1767f91b07845daf5d3e2b9ee0b160f52264ed1b9f0ff725147d59ade3'

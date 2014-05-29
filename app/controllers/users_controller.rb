85f69f60bacd9fa40343c02ed8112fa54246fccb
Jason Mannon <jmannonpdx@gmail.com>
2014-05-29 14:49:22 -0700

    disabled email on sign up for testing purposes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ActiveRecord::Base
   def create
     @user = User.new(params[:user])
     if @user.save
-      UserMailer.signup_confirmation(@user).deliver
+      #UserMailer.signup_confirmation(@user).deliver
       redirect_to @user, notice: "Signed up successfully."
     else
       render :new
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  after_create :send_welcome_message
+  #after_create :send_welcome_message
   def send_welcome_message
     UserMailer.signup_confirmation(self).deliver
   end
